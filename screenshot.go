4112801542f2f71c0c3344f42c0f384840e4a73e
Ahmed <ahmed29329@gmail.com>
2019-10-19 00:24:20 +0200

    move to iphone emulator

--- screenshot.go
+++ screenshot.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"math"
 	"time"
 
 	"github.com/chromedp/cdproto/emulation"
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
+	"github.com/chromedp/chromedp/device"
 )
 
 func TweetScreenShot(username string, tweetId string) (string, error) {
@@ -34,18 +34,23 @@ func TweetScreenShot(username string, tweetId string) (string, error) {
 }
 
 func TweetSendReply(userScreenName, tweetID, message string) error {
-	chromedpContext, cancelCtxt := chromedp.NewContext(context.Background()) // create new tab
+	opts := append(
+		chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", true),
+	)
+	context, contextCancel := context.WithTimeout(context.Background(), 50*time.Second)
+	defer contextCancel()
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context, opts...)
+	defer cancel()
+
+	chromedpContext, cancelCtxt := chromedp.NewContext(allocCtx) // create new tab
 	defer cancelCtxt()
 
 	var buf []byte
 	if err := chromedp.Run(chromedpContext, replyToTweet(userScreenName, tweetID, message, &buf)); err != nil {
 		return err
 	}
-
-	if err2 := ioutil.WriteFile("./pics/fullScreenshot-2222.png", buf, 0644); err2 != nil {
-		log.Fatal(err2)
-	}
-
 	return nil
 }
 
@@ -60,25 +65,22 @@ func elementScreenshot(urlstr, sel string, res *[]byte) chromedp.Tasks {
 
 func replyToTweet(userScreen, tweetId, message string, res *[]byte) chromedp.Tasks {
 	replyIcon := "document.querySelector('[aria-label=\"Reply\"]')"
-	replyTextAreaPlaceHolder := "document.querySelector(\"#react-root > div > div > div.r-1d2f490.r-u8s1d.r-zchlnj.r-ipm5af.r-184en5c > div > div > div > div > div.css-1dbjc4n.r-1habvwh.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-t23y2h.r-1wbh5a2.r-rsyp9y.r-1pjcn9w.r-htvplk.r-1udh08x.r-1potc6q > div > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div > div > div:nth-child(2) > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-46vdb2.r-15d164r.r-9cviqr.r-bcqeeo.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div.css-1dbjc4n.r-184en5c > div > div > div > div > div > div > div > div > div.css-901oao.r-hkyrab.r-6koalj.r-16y2uox.r-1qd0xha.r-1b6yd1w.r-16dba41.r-ad9z0x.r-bcqeeo.r-qvutc0 > div > div > div > div.DraftEditor-editorContainer > div > div > div > div\")"
-	replyTextAreaInput := "document.querySelector(\"#react-root > div > div > div.r-1d2f490.r-u8s1d.r-zchlnj.r-ipm5af.r-184en5c > div:nth-child(1) > div > div > div > div.css-1dbjc4n.r-1habvwh.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-t23y2h.r-1wbh5a2.r-rsyp9y.r-1pjcn9w.r-htvplk.r-1udh08x.r-1potc6q > div > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div > div > div:nth-child(2) > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-46vdb2.r-15d164r.r-9cviqr.r-bcqeeo.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div.css-1dbjc4n.r-184en5c > div > div > div > div > div > div > div > div > div.css-901oao.r-hkyrab.r-6koalj.r-16y2uox.r-1qd0xha.r-1b6yd1w.r-16dba41.r-ad9z0x.r-bcqeeo.r-qvutc0 > div > div > div > div.DraftEditor-editorContainer > div\")"
-	replyButton := "document.querySelector(\"#react-root > div > div > div.r-1d2f490.r-u8s1d.r-zchlnj.r-ipm5af.r-184en5c > div:nth-child(1) > div > div > div > div.css-1dbjc4n.r-1habvwh.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-t23y2h.r-1wbh5a2.r-rsyp9y.r-1pjcn9w.r-htvplk.r-1udh08x.r-1potc6q > div > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div > div > div:nth-child(2) > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-46vdb2.r-15d164r.r-9cviqr.r-bcqeeo.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div:nth-child(2) > div > div > div:nth-child(2) > div.css-18t94o4.css-1dbjc4n.r-urgr8i.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-1w2pmg.r-1n0xq6e.r-1vuscfd.r-1dhvaqw.r-1fneopy.r-o7ynqc.r-6416eg.r-lrvibr\")"
+	replyTextAreaInput := "document.querySelector(\"#react-root > div > div > div > main > div > div > div.css-1dbjc4n.r-16y2uox.r-1jgb5lz.r-13qz1uu > div > div:nth-child(2) > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-46vdb2.r-hxarbt.r-9cviqr.r-bcqeeo.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div.css-1dbjc4n.r-184en5c > div > div > div > div > div > div > div > div > div.css-901oao.r-hkyrab.r-6koalj.r-16y2uox.r-1qd0xha.r-1i10wst.r-16dba41.r-ad9z0x.r-bcqeeo.r-qvutc0 > textarea\")"
+	replyButton := "document.querySelector(\"#react-root > div > div > div.css-1dbjc4n.r-1pi2tsx.r-13qz1uu.r-417010 > main > div > div > div.css-1dbjc4n.r-ahm1il.r-136ojw6 > div > div > div > div.css-1dbjc4n.r-obd0qt.r-1pz39u2.r-1777fci.r-1uvorsx.r-174vidy.r-18bj3io > div\")"
 
 	_ = replyIcon
-	_ = replyTextAreaPlaceHolder
 	_ = replyTextAreaInput
 	_ = replyButton
-
+	logAndPrint("perpare actions")
 	replyTweetActions := []chromedp.Action{
 		chromedp.Navigate(fmt.Sprintf("https://mobile.twitter.com/%s/status/%s", userScreen, tweetId)),
-		chromedp.Sleep(time.Second * 3),
+		chromedp.Reload(),
 		chromedp.WaitVisible(replyIcon, chromedp.ByJSPath),
 		chromedp.Click(replyIcon, chromedp.ByJSPath),
-		chromedp.WaitVisible(replyTextAreaPlaceHolder, chromedp.ByJSPath),
+		chromedp.WaitVisible(replyTextAreaInput, chromedp.ByJSPath),
 		chromedp.SendKeys(replyTextAreaInput, message, chromedp.ByJSPath),
 		chromedp.Click(replyButton, chromedp.ByJSPath),
 		chromedp.Sleep(time.Second * 3),
-		fullScreenShot(res),
 	}
 	_ = replyTweetActions
 	actions := []chromedp.Action{}
@@ -98,12 +100,13 @@ func loginFirst(res *[]byte) []chromedp.Action {
 	_ = emailInputJSPath
 	_ = passwordInputJSPath
 	return []chromedp.Action{
+		chromedp.Emulate(device.IPhone7),
 		chromedp.Navigate("https://mobile.twitter.com/login"),
 		chromedp.WaitVisible(loginButtonJSPath, chromedp.ByJSPath),
 		chromedp.SendKeys(emailInputJSPath, TWITTER_USER, chromedp.ByJSPath),
 		chromedp.SendKeys(passwordInputJSPath, TWITTER_PASS, chromedp.ByJSPath),
 		chromedp.Click(loginButtonJSPath, chromedp.ByJSPath),
-		chromedp.Sleep(time.Second * 3),
+		chromedp.Sleep(time.Second * 5),
 	}
 }
 
